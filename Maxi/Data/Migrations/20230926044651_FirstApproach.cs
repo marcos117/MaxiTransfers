f2e9a6b500b5d2a23238a3548ecc464f2244dc2d
MarcosLuevano <mluevanogonzalez@gmail.com>
2023-09-25 22:47:54 -0600

    Add firstApproach for migrations

---
+++ Maxi/Data/Migrations/20230926044651_FirstApproach.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Data.Migrations
+{
+    public partial class FirstApproach : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "BeneficiaryData",
+                columns: table => new
+                {
+                    Name = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DateOfBirth = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Curp = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Ssn = table.Column<int>(type: "int", nullable: false),
+                    Phone = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Nationality = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ParticipationPercentage = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BeneficiaryData", x => x.Name);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "EmployeeData",
+                columns: table => new
+                {
+                    EmployeeNumber = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DateOfBirth = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Curp = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Ssn = table.Column<int>(type: "int", nullable: false),
+                    Phone = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Nationality = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EmployeeData", x => x.EmployeeNumber);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BeneficiaryData");
+
+            migrationBuilder.DropTable(
+                name: "EmployeeData");
+        }
+    }
+}
